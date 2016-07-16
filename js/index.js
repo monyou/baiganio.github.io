6adc4f768bb688de0fba0a9b0f534fba755867b3
BaiGanio <big_l@abv.bg>
2016-07-16 15:11:54 +0300

    Info about Bai Ganio

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Republic Of Gamers on 7/16/2016.
+ */
+
+$('#ganio-info').click(function () {
+    document.getElementById('main-content').style.display = 'inline-block';
+    document.getElementById('bai-ganio').style.display = 'block';
+    document.getElementById('bai-ganio').style.width = '65%';
+    $('#bai-ganio').css('margin', '0 auto');
+
+});
+
+
