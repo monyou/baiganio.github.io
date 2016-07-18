62526858696335f1d95786fd4b0b0eb7cc3091fd
BaiGanio <big_l@abv.bg>
2016-07-18 15:39:16 +0300

    fixed loading and append issues

--- Space University/scripts/main-page-content.js
+++ Space University/scripts/main-page-content.js
@@ -13,7 +13,16 @@ function showAJAXError (data, status) {
 function showError(msgText) {
     $('#errorBox').text(msgText).show();
 }
+function showInfo(msgText) {
+    $('#infoBox').text(msgText).show().delay(3000).fadeOut(800);
+}
 
+function showBooksAjaxError(data, status) {
+    let errorMsg = 'Error: ' + JSON.stringify(data);
+    // or
+    let errorMsgBk = "You need to be logged...";
+    showError(errorMsgBk);
+}
 
 function showView(viewID) {
     /*
@@ -27,14 +36,43 @@ $(function () {
     $('#linkHome').click(showHomeView);
     $('#linkLogin').click(showLoginView);
     $('#linkRegister').click(showRegisterView);
-    $('#linkCreatePost').click(showCreatePostView);
     $('#linkListPosts').click(showListPostsView);
+    $('#linkCreatePost').click(showCreatePostView);
     $('#linkLogout').click(logout);
+
+   /* Note that by default HTML forms submit their data as HTTP GET request.
+    You should prevent this default action and replace it with JavaScript code.
+    Use e.preventDefault() as shown above.
+    Otherwise, the form will sometimes execute your JavaScript code,
+    sometimes will post its data as HTTP GET request.
+    */
+    $('#loginForm').submit(function (e) {
+        e.preventDefault();
+        login();
+    });
+    $('#registerForm').submit(function (e) {
+        e.preventDefault();
+        register();
+    });
+    $('#createPostForm').submit(function (e) {
+        e.preventDefault();
+        createPost();
+    });
 
     $('#loginButton').click(login);
     $('#registerButton').click(register);
     $('#createPostButton').click(createPost);
-    
+
+    /* Attach AJAX "loading" event listener*/
+    $(document).on({
+       ajaxStart: function () {
+           $('#loadingBox').show();
+       },
+        ajaxStop: function () {
+            $('#loadingBox').hide();
+        }
+    });
+
     showHomeView();
     showHideNavLinks();
 });
@@ -42,6 +80,7 @@ $(function () {
 /* Choose what links to be seen when user is logged or not */
 function showHideNavLinks() {
     let loggedIn = sessionStorage.authToken != null;
+
     if(loggedIn){
         $('#linkLogin').hide();
         $('#linkRegister').hide();
@@ -69,7 +108,7 @@ function login() {
         username : $('#loginUsername').val(),
         password : $('#loginPassword').val()
     };
-    alert('IN LOGIN');
+    // alert('IN LOGIN');
     $.ajax({
         method: "POST",
         url:kinveyServiceBaseUrl + 'user/' + kinveyAppID + '/login',
@@ -82,17 +121,16 @@ function login() {
     });
 
     function loginSuccess(data, status) {
-        sessionStorage.authToken = data._kmd.authToken;
+
+        sessionStorage.authToken = data._kmd.authtoken;
         showListPostsView();
         showHideNavLinks();
 
         showInfo('Login successful');
-    };
-};
-
-function showInfo(msgText) {
-    $('#infoBox').text(msgText).show().delay(3000).fadeOut();
+    }
 }
+
+
 
 
 function showRegisterView() {
@@ -107,11 +145,42 @@ function showCreatePostView() {
 function createPost() {
     
 }
+
+let appended = false;
 function showListPostsView() {
     showView('viewListPosts');
+    $.ajax({
+        method: "GET",
+        url:kinveyServiceBaseUrl + 'appdata/' + kinveyAppID + '/posts',
+        headers: {
+            "Authorization": "Kinvey " + sessionStorage.authToken
+        },
+        success: booksLoaded,
+        error: showBooksAjaxError
+    });
+
+    function booksLoaded(data, status) {
+        if(!appended && sessionStorage.authToken != null){
+            for(let book of data){
+                $('#previevwHolder')
+                    .append($('<div class="col-md-6">')
+                        .append($('<h2 class="guildof"></h2>').text(book.Title))
+                        .append($('<h4 class="buxtonSketch"></h4>').text(book.Description))
+                        .append($("<img>", {class: 'postImg', src: book.ImgUrl}))
+                        .append($("<br>"))
+                        .append($("<br>"))
+                        .append($("<a>", {class: 'postUrl', href: book.ArticleUrl, target: "_blank"}).text("Read more..."))
+                    );
+            }
+            appended = true;
+        }
+        showInfo('Posts loaded.');
+    }
 }
 function logout() {
     sessionStorage.clear();
+    $('#previevwHolder').empty();
+    appended = false;
     showHideNavLinks();
     showHomeView();
 }
