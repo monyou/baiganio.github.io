da5985dc5bc335c499b3d55fd412afbcea0e09f9
BaiGanio <big_l@abv.bg>
2016-07-18 03:58:16 +0300

    some nav style shits

--- Space University/scripts/side-navigation.js
+++ Space University/scripts/side-navigation.js
@@ -5,13 +5,37 @@
  and add a black background color to body */
 function openNav() {
     let maxwidth = $(window).width();
-    if(maxwidth < 361){
-        document.getElementById("mySidenav").style.width = "70%";
-        document.getElementById("main").style.marginLeft = "70%";
+    let maxheght = $(window).height();
+    if(maxwidth <= 321){
+        document.getElementById("mySidenav").style.width = "94%";
+        // document.getElementById("main").style.marginLeft = "70%";
+        // document.getElementById("footer-holder").style.marginLeft = "70%";
+        // document.body.style.backgroundColor = "rgba(0,0,0,0.4)"
+    }
+    else if(maxwidth >= 320 && maxwidth < 360){
+        document.getElementById("mySidenav").style.width = "90%";
+    }
+    else if(maxwidth >= 360 && maxwidth < 800){
+        document.getElementById("mySidenav").style.width = "90%";
+        document.getElementById("main").style.marginLeft = "90%";
+        document.getElementById("footer-holder").style.marginLeft = "90%";
+    }
+    else if(maxwidth > 800 && maxwidth < 980){
+        document.getElementById("mySidenav").style.width = "27%";
+        document.getElementById("main").style.marginLeft = "27%";
+        document.getElementById("footer-holder").style.marginLeft = "27%";
         document.body.style.backgroundColor = "rgba(0,0,0,0.4)"
-    }else{
-        document.getElementById("mySidenav").style.width = "20%";
-        document.getElementById("main").style.marginLeft = "20%";
+    }
+    else if(maxwidth >= 980 && maxwidth < 1280){
+        document.getElementById("mySidenav").style.width = "25%";
+        document.getElementById("main").style.marginLeft = "25%";
+        document.getElementById("footer-holder").style.marginLeft = "25%";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)"
+    }
+    else{
+        document.getElementById("mySidenav").style.width = "30%";
+        document.getElementById("main").style.marginLeft = "30%";
+        document.getElementById("footer-holder").style.marginLeft = "30%";
         document.body.style.backgroundColor = "rgba(0,0,0,0.4)"
 
     }
@@ -27,5 +51,8 @@ function closeNav() {
     // console.log(mo);
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("footer-holder").style.margin = "0 auto";
+
+
     document.body.style.backgroundColor = "white";
 }
