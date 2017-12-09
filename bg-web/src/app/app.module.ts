d6ca96e9e4a4b57bf16bf329510e50d9eecdf671
BaiGanio <lubenkikov@gmail.com>
2017-12-09 19:41:58 +0200

    Basic componets created.

--- bg-web/src/app/app.module.ts
+++ bg-web/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HiddenSideNavComponent } from './hidden-side-nav/hidden-side-nav.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HiddenSideNavComponent,
+    SidebarComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
---
+++ bg-web/src/app/hidden-side-nav/hidden-side-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hidden-side-nav',
+  templateUrl: './hidden-side-nav.component.html',
+  styleUrls: ['./hidden-side-nav.component.css']
+})
+export class HiddenSideNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
