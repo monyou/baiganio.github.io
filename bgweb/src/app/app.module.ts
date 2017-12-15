a1a1f0720ca4136c773eba822c0098eb33693e41
BaiGanio <lubenkikov@gmail.com>
2017-12-15 17:37:10 +0200

    Added few pages-components.

--- bgweb/src/app/app.module.ts
+++ bgweb/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { SidebarComponent } from './blueprint/sidebar/sidebar.component';
 import { MainSectionComponent } from './blueprint/main-section/main-section.component';
 import { FooterComponent } from './blueprint/footer/footer.component';
 import { SidebarHiddenComponent } from './blueprint/sidebar-hidden/sidebar-hidden.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { FakeComponent } from './pages/fake/fake.component';
 
 
 @NgModule({
@@ -15,7 +17,9 @@ import { SidebarHiddenComponent } from './blueprint/sidebar-hidden/sidebar-hidde
     SidebarComponent,
     MainSectionComponent,
     FooterComponent,
-    SidebarHiddenComponent
+    SidebarHiddenComponent,
+    LandingComponent,
+    FakeComponent
   ],
   imports: [
     BrowserModule
