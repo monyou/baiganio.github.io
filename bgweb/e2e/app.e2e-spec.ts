576230e9760945eb8d76c273308a1823ffe111ad
BaiGanio <lubenkikov@gmail.com>
2017-12-15 18:46:48 +0200

    Did some refinements.

--- bgweb/e2e/app.e2e-spec.ts
+++ bgweb/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('bgweb App', () => {
+describe('baiganio.github.io App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- bgweb/src/app/app.module.ts
+++ bgweb/src/app/app.module.ts
@@ -3,23 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './blueprint/sidebar/sidebar.component';
 import { MainSectionComponent } from './blueprint/main-section/main-section.component';
-import { FooterComponent } from './blueprint/footer/footer.component';
-import { SidebarHiddenComponent } from './blueprint/sidebar-hidden/sidebar-hidden.component';
-import { LandingComponent } from './pages/landing/landing.component';
-import { FakeComponent } from './pages/fake/fake.component';
+import { SidebarComponent } from './blueprint/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
     MainSectionComponent,
-    FooterComponent,
-    SidebarHiddenComponent,
-    LandingComponent,
-    FakeComponent
+    SidebarComponent
   ],
   imports: [
     BrowserModule
