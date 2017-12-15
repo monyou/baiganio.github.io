d43ca1add4afa7e574e394cb5a795bdba96e4fd1
BaiGanio <lubenkikov@gmail.com>
2017-12-15 17:35:49 +0200

    Included `blueprint` components

--- bgweb/src/app/app.module.ts
+++ bgweb/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './blueprint/sidebar/sidebar.component';
+import { MainSectionComponent } from './blueprint/main-section/main-section.component';
+import { FooterComponent } from './blueprint/footer/footer.component';
+import { SidebarHiddenComponent } from './blueprint/sidebar-hidden/sidebar-hidden.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    MainSectionComponent,
+    FooterComponent,
+    SidebarHiddenComponent
   ],
   imports: [
     BrowserModule
---
+++ bgweb/src/app/blueprint/sidebar-hidden/sidebar-hidden.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-hidden',
+  templateUrl: './sidebar-hidden.component.html',
+  styleUrls: ['./sidebar-hidden.component.css']
+})
+export class SidebarHiddenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
