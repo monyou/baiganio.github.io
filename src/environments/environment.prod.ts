a01365202c8c3e90130271571aed5a936161d052
Lyuben Kikov <exceptionhelper@gmail.com>
2018-11-16 14:06:14 +0200

    Updated prod credentials for bgv1.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,21 +1,21 @@
 
 export const environment = {
     production: true,
-    appUrl: 'https://baiganio.github.io/',
+    appUrl: 'https://bgv1.azurewebsites.net',
     apiUrl: 'https://bgapi.azurewebsites.net/api/',
     idsUrl: 'https://ids4core20.azurewebsites.net/',
 
     // IPCheckingServiceUrl: 'https://ipinfo.io?token=$2e07b81e6d80f2',
     IPCheckingServiceUrl: 'https://api.ipify.org?format=json',
     identityServerClientCredentials: {
-        client_id: 'bgwebapp-client',
-        client_secret: '8526cfb2-472b-4c40-b704-51d0dbf9edc3',
+        client_id: 'bgv1webapp-client',
+        client_secret: '2179605b-35fb-46bd-b241-2f502229aa9a',
         scope: 'scope.bgapi',
         grant_type: 'client_credentials'
     },
     identityServerUserCredentials: {
-        client_id: 'bgwebapp-user',
-        client_secret: '8eb2c29f-c0f9-4020-a165-aaf54333aba3t',
+        client_id: 'bgv1webapp-user',
+        client_secret: 'c9994578-08fd-4a77-81c4-12b4fce035bb',
         scope: 'scope.bgapi',
         grant_type: 'password'
     },
