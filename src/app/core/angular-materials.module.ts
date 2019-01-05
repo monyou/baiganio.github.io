3aecec87e69e4f403ce2db8bb4979049db2ad6f6
Симеон Мечков <montishak@gmail.com>
2019-01-05 15:24:13 +0200

    added basic auth component and angular material

---
+++ src/app/core/angular-materials.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '../../../node_modules/@angular/core';
+import {
+    MatInputModule, MatFormFieldModule, MatButtonModule, MatAutocompleteModule, MatBadgeModule, MatBottomSheetModule,
+    MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatDatepickerModule,
+    MatDialogModule, MatDividerModule, MatExpansionModule, MatGridListModule, MatIconModule,
+    MatListModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule, MatProgressBarModule,
+    MatProgressSpinnerModule, MatRadioModule, MatRippleModule, MatSidenavModule, MatSliderModule,
+    MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatStepperModule, MatTableModule,
+    MatTabsModule, MatToolbarModule, MatTooltipModule, MatTreeModule
+} from '@angular/material';
+import { MatSelectModule } from '@angular/material/select';
+
+@NgModule({
+    exports: [
+        MatAutocompleteModule,
+        MatBadgeModule,
+        MatFormFieldModule,
+        MatBottomSheetModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatChipsModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatExpansionModule,
+        MatGridListModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
+        MatMenuModule,
+        MatNativeDateModule,
+        MatPaginatorModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatRadioModule,
+        MatRippleModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatSnackBarModule,
+        MatSortModule,
+        MatStepperModule,
+        MatTableModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatTooltipModule,
+        MatTreeModule,
+    ]
+})
+export class CustomAngularMaterialsModule {
+}
